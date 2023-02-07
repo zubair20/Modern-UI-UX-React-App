73505d7f0e4b989407ab162524b1165f3fe81196
zubair20 <zubair.akhtar20@gmail.com>
2023-02-08 04:39:39 +0500

    text changed

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Let's Build Something amazing with GPT-3 OpenAI
+          Let's Build Something amazing with GPT-3 OpenAI testing
         </h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <div className="gpt3__header-content__input">
